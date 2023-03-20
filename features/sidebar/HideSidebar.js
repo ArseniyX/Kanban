f572ea36d35f0516ed23e922e267695a3ffc01c8
ArseniyDev <arseniy@tiktuk.com>
2023-03-20 08:44:54 +0200

    fix

--- features/sidebar/HideSidebar.js
+++ features/sidebar/HideSidebar.js
@@ -2,7 +2,7 @@ import { html } from 'preact'
 import Tab from '../../common/Tab.js'
 import { toggleSidebar } from '../../store/rootStore.js'
 
-const Icon = () => html`<img src="../assets/icon-hide-sidebar.svg" />`
+const Icon = () => html`<img src="./assets/icon-hide-sidebar.svg" />`
 
 const HideSidebar = () => {
     return html`<${Tab} handler="${toggleSidebar}" title="Hide Sidebar" Icon="${Icon}" />`
